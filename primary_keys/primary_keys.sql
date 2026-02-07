d40f53bb400c8e1986d4a5c10bc3e9348e00834d
codewithsyre <augustineshoks7@gmail.com>
2026-02-07 13:39:38 +0200

    feat: Remove `repo` table creation and data insertion from `primary_keys.sql`.

--- primary_keys/primary_keys.sql
+++ primary_keys/primary_keys.sql
@@ -29,16 +29,3 @@ CREATE TABLE product_tags (
   tag_id INT,
   PRIMARY KEY (product_id, tag_id)
 );
-
-CREATE TABLE repo (
-  url VARCHAR(255),
-  commit_hash VARCHAR(255),
-  PRIMARY KEY (url, commit_hash)
-);
-
-INSERT INTO
-  repo (url, commit_hash)
-VALUES
-  ('https://github.com/avgvstinee/SQL-essentials.git', 'commit_hash_1')
-RETURNING
-  *;
